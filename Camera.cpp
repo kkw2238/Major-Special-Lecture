e7bec60e94fb142fedc99decadb089c6db368f9d
kkw2238@naver.com <kkw2238@naver.com>
2021-05-11 20:13:34 +0900

    Commit Test
    Commit Test

--- Camera.cpp
+++ Camera.cpp
@@ -12,6 +12,7 @@ Camera::Camera(float winWidth, float winHeight)
 	//m_mat4x4Proj = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 2.0f);
 	m_mat4x4Proj = glm::perspectiveFovRH(45.0f, winWidth, winHeight, 0.0f, 10.0f);
 	m_mat4x4ProjView = m_mat4x4Proj * m_mat4x4View;
+	// Test
 }
 
 
